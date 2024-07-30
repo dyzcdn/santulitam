e881ae210e37c6ae0c2386849b727ebebe713339
dyzulk <dyzulk03@gmail.com>
2024-07-30 19:49:29 +0700

    first commit

---
+++ app/Filament/Pages/Scaner.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+use App\Filament\Pages\Scan;
+
+class Scaner extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.scan';
+}
---
+++ resources/views/filament/pages/scan.blade.php
@@ -0,0 +1,41 @@
+<x-filament-panels::page>
+    <h1>Hallo Semua</h1>
+    <div class=" w-0.9 justify-content-center">
+        <div id="reader"></div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title
+                    ">Card title</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @push('scripts')
+    <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
+    <script>
+        function onScanSuccess(decodedText, decodedResult) {
+        // handle the scanned code as you like, for example:
+        // console.log(`Code matched = ${decodedText}`, decodedResult);
+        $('#result').val(decodedText);
+        }
+
+        function onScanFailure(error) {
+        // handle scan failure, usually better to ignore and keep scanning.
+        // for example:
+        console.warn(`Code scan error = ${error}`);
+        }
+
+        let html5QrcodeScanner = new Html5QrcodeScanner(
+        "reader",
+        { fps: 10, qrbox: {width: 250, height: 250} },
+        /* verbose= */ false);
+        html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+    </script>
+    @endpush
+</x-filament-panels::page>
