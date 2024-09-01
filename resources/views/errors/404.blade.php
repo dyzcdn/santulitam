b8d3407c210a55dbb32477e24374b76a148c60db
dyzcdn <dyzulkdeveloper@gmail.com>
2024-09-01 12:07:26 +0700

    Publish v.1.0.2 release

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', __('We can’t find that page'))
+@section('code', '404')
+@section('message', __('Sorry, the page you are looking for doesn\'t exist or has been moved.'))
