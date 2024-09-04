a9cba90157192d94b56bee1d1adca8bf9e64f462
Dyzulk Developer <136977118+dyzcdn@users.noreply.github.com>
2024-09-04 14:52:43 +0700

    Update ApkController.php

--- app/Http/Controllers/ApkController.php
+++ app/Http/Controllers/ApkController.php
@@ -9,24 +9,28 @@ class ApkController extends Controller
 {
     public function index()
     {
-        // Ambil semua data student jika diperlukan di view
-        // $students = Student::all();
         return view('apk');
     }
 
     public function download(Request $request)
     {
-
         $filePath1 = 'android/Sakaritam-Mobile-v1.0.0.apk';
         $filePath2 = 'android/Sakaritam-Lite-v.1.0.0.apk';
 
         // Cek apakah file ada di storage (disk 'public')
         if ($request->info == 1) {
-            return Storage::disk('public')->download($filePath1);
+            $filePath = $filePath1;
         } elseif($request->info == 0) {
-            return Storage::disk('public')->download($filePath2);
+            $filePath = $filePath2;
         } else {
             return redirect()->route('download-qr.index')->with('danger', 'QR dengan NIM tersebut tidak ditemukan.');
         }
+
+        $fileName = basename($filePath); // Mendapatkan nama file dengan ekstensi
+        $headers = [
+            'Content-Type' => 'application/vnd.android.package-archive', // Menetapkan Content-Type khusus APK
+        ];
+
+        return Storage::disk('public')->download($filePath, $fileName, $headers);
     }
 }
